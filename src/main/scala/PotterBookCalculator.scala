e1751c826573d663a845db52376bcb88ca89dc6f
Kristoffer Karlsson <karlsson.kristoffer93@gmail.com>
2019-04-16 23:01:13 +0200

    Minor improvment on potterbook

--- src/main/scala/PotterBookCalculator.scala
+++ src/main/scala/PotterBookCalculator.scala
@@ -16,7 +16,7 @@ class PotterBookCalculator {
   def calculate(books: Seq[PotterBook]): Double = {
 
     if (books.length == 2) {
-      if (books.distinct.length == 2)  (8 * 2) * 0.95
+      if (books.distinct.length == 2) (8 * 2) * 0.95
       else 8
     }
     else if (books.length == 3) (8 * 3) * 0.90
@@ -30,11 +30,11 @@ class PotterBookCalculator {
 
 /*
 The potter book calculator kata
-1 book 8 kr
+1 book 8 euro
 2 books 5 % discount
 3 books 10 % discount
 4 books 20 % discount
 5 books % 25 percent discount
-
-the discount is only valid for sets of books. Buying 2 of one book is not valid
+the discount is only valid for sets of books. Buying 2 of one book is not valid.
+But buying 2 of each book is 2 sets = valid
  */
